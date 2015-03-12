6f127a7eb013a3e2814f04988415855455940771
JeffersonH44 <swrt_9950@hotmail.com>
2015-03-12 12:15:12 -0500

    Regular class created.

---
+++ src/org/vnotebook/versioning/Regular.java
@@ -0,0 +1,31 @@
+package org.vnotebook.versioning;
+
+public class Regular extends User{
+    private int postViews;
+    private int strikesNumber;
+    private int starsNumber;
+
+    public int getPostViews() {
+        return postViews;
+    }
+
+    public int getStrikesNumber() {
+        return strikesNumber;
+    }
+
+    public int getStarsNumber() {
+        return starsNumber;
+    }
+
+    public void setPostViews(int postViews) {
+        this.postViews = postViews;
+    }
+
+    public void setStrikesNumber(int strikesNumber) {
+        this.strikesNumber = strikesNumber;
+    }
+
+    public void setStarsNumber(int starsNumber) {
+        this.starsNumber = starsNumber;
+    }
+}
