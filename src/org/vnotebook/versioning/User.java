b0c63b4981649660c8f7f1a3a1a221e5ef084505
JeffersonH44 <swrt_9950@hotmail.com>
2015-03-12 12:11:22 -0500

    User class created.

---
+++ src/org/vnotebook/versioning/User.java
@@ -0,0 +1,49 @@
+package org.vnotebook.versioning;
+
+public class User {
+    private String name;
+    private String lastName;
+    private int age;
+    private String username;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
