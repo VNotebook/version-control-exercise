7e9e57ec3ae385aa3b1b8e412537cf077d7e2f56
Daniel Castro Alvarado <dancas2@gmail.com>
2015-03-12 12:11:29 -0500

    Adding Post class

---
+++ src/org/vnotebook/versioning/Post.java
@@ -0,0 +1,42 @@
+package org.vnotebook.versioning;
+
+import java.util.Date;
+
+public class Post {
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public boolean isAllowed() {
+        return isAllowed;
+    }
+
+    public void setAllowed(boolean isAllowed) {
+        this.isAllowed = isAllowed;
+    }
+
+    private String topic;
+    private Date dateCreated;
+    private Date lastUpdate;
+    private boolean isAllowed;
+}
