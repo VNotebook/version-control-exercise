2a4427eb3f4390b6d41afb3cb8245124616e530f
Milder Hernandez <milderhc@gmail.com>
2015-03-12 23:32:47 -0500

    Admin class created

---
+++ src/org/vnotebook/versioning/Admin.java
@@ -0,0 +1,43 @@
+package org.vnotebook.versioning;
+
+import java.util.ArrayList;
+
+public class Admin extends User {
+    private int level;
+    private double rating;
+	private ArrayList<Forum> forumsToModerate;
+    
+    public Admin ( int level, double rating ) {
+        this.level = level;
+        this.rating = rating;
+        forumsToModerate = new ArrayList<>();
+    }
+    
+    public Admin ( ) {
+        forumsToModerate = new ArrayList<>();
+    }
+    
+    public void addForum ( Forum forum ) {
+        forumsToModerate.add(forum);
+    }
+    
+    public ArrayList<Forum> getForums ( ) {
+        return forumsToModerate;
+    }
+    
+    public int getLevel ( ) {
+        return level;
+    }
+    
+    public double getRating ( ) {
+        return rating;
+    }
+    
+    public void setLevel ( int level ) {
+        this.level = level;
+    }
+    
+    public void setRanting ( double rating ) {
+        this.rating = rating;
+    }
+}
