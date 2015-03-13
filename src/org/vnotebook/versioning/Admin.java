6ce9fd6729a2ec8f60c2be0a2393c5e10e06397e
Daniel Castro Alvarado <dancas2@gmail.com>
2015-03-12 23:38:28 -0500

    Minor fixes in Admin

--- src/org/vnotebook/versioning/Admin.java
+++ src/org/vnotebook/versioning/Admin.java
@@ -5,39 +5,39 @@ import java.util.ArrayList;
 public class Admin extends User {
     private int level;
     private double rating;
-	private ArrayList<Forum> forumsToModerate;
+    private ArrayList<Forum> forumsToModerate;
     
-    public Admin ( int level, double rating ) {
+    public Admin(int level, double rating) {
         this.level = level;
         this.rating = rating;
         forumsToModerate = new ArrayList<>();
     }
     
-    public Admin ( ) {
+    public Admin() {
         forumsToModerate = new ArrayList<>();
     }
     
-    public void addForum ( Forum forum ) {
+    public void addForum(Forum forum) {
         forumsToModerate.add(forum);
     }
     
-    public ArrayList<Forum> getForums ( ) {
+    public ArrayList<Forum> getForums() {
         return forumsToModerate;
     }
     
-    public int getLevel ( ) {
+    public int getLevel() {
         return level;
     }
     
-    public double getRating ( ) {
-        return rating;
-    }
-    
-    public void setLevel ( int level ) {
+    public void setLevel(int level) {
         this.level = level;
     }
     
-    public void setRanting ( double rating ) {
+    public double getRating() {
+        return rating;
+    }
+    
+    public void setRating(double rating) {
         this.rating = rating;
     }
 }
